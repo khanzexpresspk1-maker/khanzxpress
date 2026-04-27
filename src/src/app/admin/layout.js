eae9dab6751c3f5204252fc836cbef9d953ed0c8
KhanZExpress.pk <khanzexpress.pk1@gmail.com>
2026-04-28 04:33:51 +0500

    Delete src/src/app/admin/layout.js

--- src/src/app/admin/layout.js
+++
@@ -1,8 +0,0 @@
-export const metadata = {
-  title: 'KhanZxpress Admin Panel',
-  description: 'KhanZxpress Admin Control Panel',
-}
-
-export default function AdminLayout({ children }) {
-  return children;
-}
